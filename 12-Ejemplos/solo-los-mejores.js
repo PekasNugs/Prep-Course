55b599ca124a982c59b0b010af2241b31b24391f
PekasNugs <victor_roa@me.com>
2022-03-18 14:23:02 -0700

    Possible challenges

---
+++ 12-Ejemplos/solo-los-mejores.js
@@ -0,0 +1,14 @@
+function obtenersololosmejores(estudiantes, nota1, nota2) {
+    // "estudiantes" es un array de objetos "estudiante". Crea un nuevo array con el nombre y apellido de los estudiantes
+    // que en sus propiedades "check1" y "check2", tengan una nota mayor o igual a "nota1" y "nota2" respectivamente
+    // Ej.
+        /* var estudiantes = [
+            {nombre; "Maritza", apellido; "Ramirez", check1; 16, check2; 8},
+            {nombre; "Luis", apellido; "Hornelas", check1; 20, check2; 20},
+            {nombre; "Octavia", apellido; "Ruiz", check1; 10, check2; 0},
+            {nombre; "Maria", apellido; "Gonzalez", check1; 17, check2; 19}
+        ]*/
+        // obtenersololosmejores(estudiantes, 15, 15);
+        // return -> ("Luis Hornelas", "Maria Gonzalez");
+        // Escribe tu código aquí:
+}
