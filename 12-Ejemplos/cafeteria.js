665651187d9424ba593ade7488dfe0de9767d13a
PekasNugs <victor_roa@me.com>
2022-03-18 22:08:22 -0700

    Primer intento

---
+++ 12-Ejemplos/cafeteria.js
@@ -0,0 +1,13 @@
+function panaderiaTulipan(compraunpan) {
+    // Elabora un algoritmo que determine tu decision para comprar un pan y un cafe
+    // Devolver el enunciado correcto
+    // Escribe tu código aquí:
+    var cafecito = "cafe con leche";
+    var panecillo = "pan de dulce";
+    var statement = "estoy bien";
+        if(statement === true) {
+            return "entra y compra un " + panecillo + " con un " + cafecito;
+        } else {
+            return "entra y compra un " + cafecito + " con un " + panecillo;
+        }
+}
