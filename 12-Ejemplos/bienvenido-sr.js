999505fe7518d75991ad56c5b0313b6c6b711319
PekasNugs <victor_roa@me.com>
2022-03-22 21:52:31 -0700

    Primer intento

--- 12-Ejemplos/bienvenido-sr.js
+++ 12-Ejemplos/bienvenido-sr.js
@@ -2,21 +2,21 @@ function bienvenidoSr(persona) {
     // La funcion recibe un objeto "persona"
     // de la forma:
         // {
-        //  nombre: 'Lionel',
-        //  apellido: 'Messi',
+        //  nombre: 'Travis',
+        //  apellido: 'Morgan',
         //  invitado: true
         //  }
     // Si tiene la propiedad "invitado" y, además, tiene las propiedades "nombre" y "apellido", tomar esos valores y retorna:
-    // "Lionel Messi, un gusto tenerlo nuevamente! Bienvenido"
+    // "Travis Morgan, un gusto tenerlo nuevamente! Bienvenido"
     // Si tiene la propiedad "invitado" y solo tiene la propiedad "apellido", retornar:
-    // "Bienvenido Sr. Messi"
+    // "Bienvenido Sr. Morgan"
     // Si tiene la propiedad "invitado" y solo tiene la propiedad "nombre", retornar:
-    // "Hola Lionel, tu mesa está lista"
+    // "Hola Travis, tu mesa está lista"
     // En caso de que no tenga la propiedad "invitado" retornar:
     // "Disculpe señor, no está invitado a la fiesta"
     // Tu código:
-    const nombre = 'Lionel';
-    const apellido = 'Messi';
+    const nombre = 'Travis';
+    const apellido = 'Morgan';
     const invitado = true
     if(nombre === true && apellido === true && invitado ===true) {
         return nombre + apellido + " un gusto tenerlo nuevamente! Bienvenido";
@@ -26,5 +26,5 @@ function bienvenidoSr(persona) {
         return "Hola " + nombre + ", tu mesa esta lista"
     } else if(invitado === false) {
         return "Disculpe señor, no esta invitado a la fiesta"
-    } 
+    }
 }
